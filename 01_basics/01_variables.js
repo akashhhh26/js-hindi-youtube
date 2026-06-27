27ab487ddca42d47b67ec89febf0af31601f7095
akashhhh26 <akashsahu36053605@gmail.com>
2026-06-27 23:56:02 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 16657
+var accountEmail = "aks0234@hmail.com"
+let accountPassword = "846834"
+accountCity = "jaipur"
+
+// accountId = 2// not allowed 
+
+ccountEmail = "aks3605@hmail.com"
+accountPassword = "846654"
+accountCity = "nagpur"
+let accountState;
+
+
+console.log(accountId);
+
+/*
+prefer not to use VAR
+because of issue in block scope and functional scope
+
+*/
+
+console.table([accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict" // treat all js code as a new version
+
+//alert ( 3 + 3) // we are using node.js not, browser
+
+console.log ( 3 
+    +
+     3) // code readbility should be high
+
+     console.log( "akash")
+
+    // various datatypes
+     let name = "akash"  //string type
+     let age = 20  // number type
+     
+
+
+
+     // number => 2 to power 53 range
+     // bigint
+     // string => ""
+     // boolean => true/false
+     // null => standalone value
+     // undefined => 
+     // symbol => unquie
+     
+     // object 
+
+    console.log(typeof undefined) // undefined
+    console.log(typeof null); // object
+
+     
+      
+
