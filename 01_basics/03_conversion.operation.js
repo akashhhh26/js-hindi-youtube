bcdf323b717d5ace9a7a867afd3f1cf78d07d912
akashhhh26 <akashsahu36053605@gmail.com>
2026-07-01 20:25:53 +0000

    conversion.operation

---
+++ 01_basics/03_conversion.operation.js
@@ -0,0 +1,80 @@
+let score = false
+
+
+//console.log( typeof score);
+//console.log( typeof (score));
+
+let valueInNumber = Number ( score)
+//console.log( typeof valueInNumber)
+//console.log( valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN  ( not a number)
+// true => 1, false => 0;
+
+
+let isLoggedIn = "hjnb"
+
+let booleanISLoggedIn = Boolean(isLoggedIn)
+//console.log( booleanISLoggedIn);
+
+
+// true => 1, false => 0
+// "" => false, "akash" => true
+
+let SomeNumber = 33
+
+let StringNumber = String(SomeNumber)
+//console.log(StringNumber)
+//console.log( typeof StringNumber)
+
+
+//================*===================*========================*============================*====================*========================
+
+//operations
+
+let value = 5
+let negValue = -value
+//console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%3)
+
+let str1 = "akash"
+let str2 = " sahu"
+
+let str3 = str1 + str2
+//console.log(str3)
+
+// console.log( "1" + 2)
+// console.log( 1 + "2")
+// console.log( "1" + 2 + 2)
+// console.log( 1 + 2 + "2")
+
+// console.log( 2*3 + 4 % 8) //not good code 
+// console.log( (2*3) + 4 % 8)  // use perenthesis mtlb bracket
+
+//console.log(true)
+//console.log(+true)
+// console.log(true+)
+//console.log(+"")
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gamecounter = 100
+gamecounter++; // prefix sum
+console.log(gamecounter)
+++gamecounter  //suffix sum
+console.log(gamecounter)
+
+// study what is suffix and prefix sum in mdn 
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
